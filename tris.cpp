2a7f09aa238b3edc8cf492463c82999f99f826bb
ginop-1 <enrico.covili@fermi.mo.it>
2020-08-15 14:21:54 +0200

    some stuff

--- tris.cpp
+++ tris.cpp
@@ -5,13 +5,20 @@
 using namespace std;
 
 void PrintGrid(string*);
-void CPUmove(string*, string, string);
+string CPUmove(string*, string, string);
 bool CheckWin(string*, string);
 
 int main()
 {
     string grid[9], playerXO, CPUXO; int startplayer, playermove, cpumove;
     for (int i=0; i<9; i++) grid[i] = " "; // cleaning array
+    cout << "\n"
+         << "1" << "|" << "2" << "|" << "3" << endl
+         << "-----" << endl
+         << "4" << "|" << "5" << "|" << "6" << endl
+         << "-----" << endl 
+         << "7" << "|" << "8" << "|" << "9" << endl
+         << "\n" ;
     cout << "Welcome to the tris game, the initial player will be random\n\n" << "Choose beetween X or O: ";
     while (true) 
     {
@@ -23,7 +30,6 @@ int main()
     else playerXO = "O"; 
     if (playerXO == "X") CPUXO = "O";
     else CPUXO = "X";
-    PrintGrid(grid);
     srand (time(NULL)); startplayer = rand() % 2 + 0; // selecting random number (0 or 1) for starting player
     while (true)
     {
@@ -51,8 +57,9 @@ int main()
         }
         if (startplayer == 1)
         {
-            CPUmove(grid, CPUXO, playerXO);
             cout << "CPU turn\n";
+            CPUmove(grid, CPUXO, playerXO);
+            CheckWin(grid, CPUXO);
             PrintGrid(grid);
             startplayer = 0;
         }
@@ -82,28 +89,73 @@ bool CheckWin(string grid[8], string XorO)
     if (grid[2] == XorO && grid[4] == XorO && grid[6] == XorO) return true; // second oblique
 }
 
-void CPUmove(string grid[8], string XorO, string enemyXO)
+string CPUmove(string grid[8], string XorO, string enemyXO)
 {
-        // Can i win? lez find out man
-    // ROWS
-    if (grid[0] == XorO && grid[1] == XorO ) grid[2] = XorO; return;
-    if (grid[1] == XorO && grid[2] == XorO ) grid[0] = XorO; return;
-    if (grid[0] == XorO && grid[2] == XorO ) grid[1] = XorO; return;
-    if (grid[3] == XorO && grid[4] == XorO ) grid[5] = XorO; return;
-    if (grid[4] == XorO && grid[5] == XorO ) grid[3] = XorO; return;
-    if (grid[3] == XorO && grid[5] == XorO ) grid[4] = XorO; return;
-    if (grid[6] == XorO && grid[7] == XorO ) grid[8] = XorO; return;
-    if (grid[7] == XorO && grid[8] == XorO ) grid[6] = XorO; return;
-    if (grid[6] == XorO && grid[8] == XorO ) grid[7] = XorO; return;
-    // COLUMNS
-    if (grid[0] == XorO && grid[3] == XorO ) grid[6] = XorO; return;
-    if (grid[1] == XorO && grid[4] == XorO ) grid[7] = XorO; return;
-    if (grid[2] == XorO && grid[5] == XorO ) grid[8] = XorO; return;
-    if (grid[0] == XorO && grid[6] == XorO ) grid[3] = XorO; return;
-    if (grid[1] == XorO && grid[7] == XorO ) grid[4] = XorO; return;
-    if (grid[2] == XorO && grid[8] == XorO ) grid[5] = XorO; return;
-    if (grid[3] == XorO && grid[6] == XorO ) grid[0] = XorO; return;
-    if (grid[4] == XorO && grid[7] == XorO ) grid[1] = XorO; return;
-    if (grid[5] == XorO && grid[8] == XorO ) grid[2] = XorO; return;
+        // TRY TO WIN IF POSSIBLE
 
+    // ROWS
+    if (grid[0] == XorO && grid[1] == XorO ) grid[2] = XorO; return XorO;
+    if (grid[1] == XorO && grid[2] == XorO ) grid[0] = XorO; return XorO;
+    if (grid[0] == XorO && grid[2] == XorO ) grid[1] = XorO; return XorO;
+    if (grid[3] == XorO && grid[4] == XorO ) grid[5] = XorO; return XorO;
+    if (grid[4] == XorO && grid[5] == XorO ) grid[3] = XorO; return XorO;
+    if (grid[3] == XorO && grid[5] == XorO ) grid[4] = XorO; return XorO;
+    if (grid[6] == XorO && grid[7] == XorO ) grid[8] = XorO; return XorO;
+    if (grid[7] == XorO && grid[8] == XorO ) grid[6] = XorO; return XorO;
+    if (grid[6] == XorO && grid[8] == XorO ) grid[7] = XorO; return XorO;
+    // COLUMNS
+    if (grid[0] == XorO && grid[3] == XorO ) grid[6] = XorO; return XorO;
+    if (grid[1] == XorO && grid[4] == XorO ) grid[7] = XorO; return XorO;
+    if (grid[2] == XorO && grid[5] == XorO ) grid[8] = XorO; return XorO;
+    if (grid[0] == XorO && grid[6] == XorO ) grid[3] = XorO; return XorO;
+    if (grid[1] == XorO && grid[7] == XorO ) grid[4] = XorO; return XorO;
+    if (grid[2] == XorO && grid[8] == XorO ) grid[5] = XorO; return XorO;
+    if (grid[3] == XorO && grid[6] == XorO ) grid[0] = XorO; return XorO;
+    if (grid[4] == XorO && grid[7] == XorO ) grid[1] = XorO; return XorO;
+    if (grid[5] == XorO && grid[8] == XorO ) grid[2] = XorO; return XorO;
+    //OBLIQUE
+    if (grid[0] == XorO && grid[4] == XorO ) grid[8] = XorO; return XorO;
+    if (grid[2] == XorO && grid[4] == XorO ) grid[6] = XorO; return XorO;
+    if (grid[0] == XorO && grid[8] == XorO ) grid[4] = XorO; return XorO;
+    if (grid[2] == XorO && grid[6] == XorO ) grid[4] = XorO; return XorO;
+    if (grid[4] == XorO && grid[6] == XorO ) grid[2] = XorO; return XorO;
+    if (grid[4] == XorO && grid[8] == XorO ) grid[0] = XorO; return XorO;
+
+        // TRY TO NOT LOSE IF POSSIBLE
+
+    // ROWS
+    if (grid[0] == enemyXO && grid[1] == enemyXO ) grid[2] = XorO; return XorO;
+    if (grid[1] == enemyXO && grid[2] == enemyXO ) grid[0] = XorO; return XorO;
+    if (grid[0] == enemyXO && grid[2] == enemyXO ) grid[1] = XorO; return XorO;
+    if (grid[3] == enemyXO && grid[4] == enemyXO ) grid[5] = XorO; return XorO;
+    if (grid[4] == enemyXO && grid[5] == enemyXO ) grid[3] = XorO; return XorO;
+    if (grid[3] == enemyXO && grid[5] == enemyXO ) grid[4] = XorO; return XorO;
+    if (grid[6] == enemyXO && grid[7] == enemyXO ) grid[8] = XorO; return XorO;
+    if (grid[7] == enemyXO && grid[8] == enemyXO ) grid[6] = XorO; return XorO;
+    if (grid[6] == enemyXO && grid[8] == enemyXO ) grid[7] = XorO; return XorO;
+    // COLUMNS
+    if (grid[0] == enemyXO && grid[3] == enemyXO ) grid[6] = XorO; return XorO;
+    if (grid[1] == enemyXO && grid[4] == enemyXO ) grid[7] = XorO; return XorO;
+    if (grid[2] == enemyXO && grid[5] == enemyXO ) grid[8] = XorO; return XorO;
+    if (grid[0] == enemyXO && grid[6] == enemyXO ) grid[3] = XorO; return XorO;
+    if (grid[1] == enemyXO && grid[7] == enemyXO ) grid[4] = XorO; return XorO;
+    if (grid[2] == enemyXO && grid[8] == enemyXO ) grid[5] = XorO; return XorO;
+    if (grid[3] == enemyXO && grid[6] == enemyXO ) grid[0] = XorO; return XorO;
+    if (grid[4] == enemyXO && grid[7] == enemyXO ) grid[1] = XorO; return XorO;
+    if (grid[5] == enemyXO && grid[8] == enemyXO ) grid[2] = XorO; return XorO;
+    //OBLIQUE
+    if (grid[0] == enemyXO && grid[4] == enemyXO ) grid[8] = XorO; return XorO;
+    if (grid[2] == enemyXO && grid[4] == enemyXO ) grid[6] = XorO; return XorO;
+    if (grid[0] == enemyXO && grid[8] == enemyXO ) grid[4] = XorO; return XorO;
+    if (grid[2] == enemyXO && grid[6] == enemyXO ) grid[4] = XorO; return XorO;
+    if (grid[4] == enemyXO && grid[6] == enemyXO ) grid[2] = XorO; return XorO;
+    if (grid[4] == enemyXO && grid[8] == enemyXO ) grid[0] = XorO; return XorO;
+    
+        // OTHER MOVES
+    
+    if (grid[4] == " ") grid[4] = XorO; return XorO;
+    if (grid[0] == " ") grid[0] = XorO; return XorO;
+    if (grid[2] == " ") grid[2] = XorO; return XorO;
+    if (grid[6] == " ") grid[6] = XorO; return XorO;
+    if (grid[8] == " ") grid[8] = XorO; return XorO;
 }
